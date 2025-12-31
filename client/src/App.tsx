8f0552bbb000a90226e2109cff4c7f13dca6bee4
tomershatz123 <tomer.shatz@gmail.com>
2025-12-31 17:50:42 +0200

    Keep socket connection after refresh

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,21 @@
+import { useEffect } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import Chat from './pages/Chat';
+import { socket } from './socket';
 
 function App() {
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    const userId = localStorage.getItem('userId');
+
+    // If we have a token but socket isn't connected, reconnect it!
+    if (token && userId && !socket.connected) {
+      socket.connect();
+      socket.emit("join", userId);
+    }
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
